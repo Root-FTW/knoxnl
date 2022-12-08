c9b5cfffc29efacd291d2a1df9fda5a1b52d4a82
RootFTW <108767514+Root-FTW@users.noreply.github.com>
2022-12-08 13:58:03 -0800

    Update setup.py
    Just add a comment to improve the reading of the code in the future, and I corrected the letter that must be capitalized

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
+# Set up the Python module called "knoxnl"
 setup(
     name="knoxnl",
     packages=find_packages(),
     version=__import__('knoxnl').__version__,
-    description="A python wrapper around the amazing KNOXSS API by Brute Logic (requires an API Key)",
+    description="A Python wrapper around the amazing KNOXSS API by Brute Logic (requires an API Key)",
     long_description=open("README.md").read(),
     author="@xnl-h4ck3r",
     url="https://github.com/xnl-h4ck3r/knoxnl",
